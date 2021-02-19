459368439657cec5980c79bb93e9112c736f6c72
fujiwara <shintaro.fujiwara@gmail.com>
2021-02-20 08:31:53 +0900

    New version.

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_datatype';
-$plugin->release = '0.1.12';
-$plugin->version = 2021022001;
+$plugin->release = '0.1.13';
+$plugin->version = 2021022002;
 $plugin->requires = 2018051710.02;
 //$plugin->maturity = MATURITY_BETA;
